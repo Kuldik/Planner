e51a6ef19afde295495e23fce1c153f3130c4d1e
Kuldik <112172212+Kuldik@users.noreply.github.com>
2024-04-14 13:57:54 +0000

    services

--- planner-frontend/src/api/interceptors.ts
+++ planner-frontend/src/api/interceptors.ts
@@ -1,6 +1,12 @@
 import axios, { type CreateAxiosDefaults } from 'axios'
 
-import { getAccessToken } from '../services/auth-token.service'
+import {
+	getAccessToken,
+	removeFromStorage
+} from '../services/auth-token.service'
+
+import { errorCatch } from './error'
+import { authService } from '@/services/auth.service'
 
 const options: CreateAxiosDefaults = {
 	baseURL: 'http://localhost:3000',
@@ -21,3 +27,29 @@ axiosWithAuth.interceptors.request.use(config => {
 
 	return config
 })
+
+axiosWithAuth.interceptors.response.use(
+	config => config,
+	async error => {
+		const originalRequest = error.config
+
+		if (
+			error?.response?.status === 401 ||
+			errorCatch(error) === 'jwt expired' ||
+			(errorCatch(error) === 'jwt must be provided' &&
+				error.config &&
+				!error.config._isRetry)
+		) {
+			originalRequest._isRetry = true
+			try {
+				await authService.getNewTokens()
+				return axiosWithAuth.request(originalRequest)
+			} catch (error) {
+				if (errorCatch(error) === 'jwt expired') removeFromStorage()
+			}
+		}
+		throw error
+	}
+)
+
+export { axiosClassic, axiosWithAuth }
--- planner-frontend/src/types/pomodoro.types.ts
+++ planner-frontend/src/types/pomodoro.types.ts
@@ -1,17 +1,17 @@
-import { IBase } from './root.types'
+import type { IBase } from './root.types'
 
 export interface IPomodoroRoundResponse extends IBase {
-	isCompleted: boolean
+	isCompleted?: boolean
 	totalSeconds: number
 }
 
 export interface IPomodoroSessionResponse extends IBase {
-	isCompleted: boolean
+	isCompleted?: boolean
 	rounds?: IPomodoroRoundResponse[]
 }
 
-export type TypePomodoroState = Partial<
-	Omit<IPomodoroRoundResponse, 'id' | 'createdAt' | 'updatedAt'>
+export type TypePomodoroSessionState = Partial<
+	Omit<IPomodoroSessionResponse, 'id' | 'createdAt' | 'updatedAt'>
 >
 
 export type TypePomodoroRoundState = Partial<
